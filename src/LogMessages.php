b9e2b357de69ef889c053e17c48b0ae49a28d206
Saksham Gupta <sakshamg1304@gmail.com>
2026-02-04 14:22:39 +0530

    Update LogMessages.php

--- src/LogMessages.php
+++ src/LogMessages.php
@@ -10,6 +10,7 @@ class LogMessages {
       "debugLogs" => json_decode(file_get_contents(__DIR__ . "/debug-messages.json"), true),
       "infoLogs" => json_decode(file_get_contents(__DIR__ . "/info-messages.json"), true),
       "errorLogs" => json_decode(file_get_contents(__DIR__ . "/error-messages.json"), true),
+      "errorLogsV2" => json_decode(file_get_contents(__DIR__ . "/error-messages-v2.json"), true),
       "warnLogs" => json_decode(file_get_contents(__DIR__ . "/warn-messages.json"), true),
       "traceLogs" => json_decode(file_get_contents(__DIR__ . "/trace-messages.json"), true),
     ];
